4c0a66bd243b20056032ff075aabed9faa6e948a
Kuneosu <kks92155784@gmail.com>
2024-04-05 13:32:54 +0900

    git init

---
+++ app/src/main/java/com/kks/sololifeproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.kks.sololifeproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
