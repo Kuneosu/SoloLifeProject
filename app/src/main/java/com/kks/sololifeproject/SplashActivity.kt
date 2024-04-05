b5ddebbe70ebe00186fb934e225ed49820a58e6e
Kuneosu <kks92155784@gmail.com>
2024-04-05 13:51:00 +0900

    Make SplashActivity

---
+++ app/src/main/java/com/kks/sololifeproject/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.kks.sololifeproject
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
---
+++ app/src/main/java/com/kks/sololifeproject/auth/IntroActivity.kt
@@ -0,0 +1,14 @@
+package com.kks.sololifeproject.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kks.sololifeproject.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_intro)
+
+    }
+}
